341f0652c7d7c6387f29d79dbf8efc0d687d285a
alikmpt8529 <117079395+alikmpt8529@users.noreply.github.com>
2024-03-09 15:46:38 +0900

    ver1.0.0

---
+++ my-express-app/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const path = require('path');
+let visitorCount = 0;
+
+io.on('connection', (socket) => {
+  visitorCount++;
+  console.log('A visitor connected. Current count:', visitorCount);
+  io.emit('visitorCount', visitorCount);
+
+  socket.on('disconnect', () => {
+    visitorCount--;
+    console.log('A visitor disconnected. Current count:', visitorCount);
+    io.emit('visitorCount', visitorCount);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
